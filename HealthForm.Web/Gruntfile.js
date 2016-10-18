539bde855dda814e67dabcb472307c55331f89de
unatani <unatani@ISS-Z211519>
2016-10-18 17:19:09 -0400

    bower and grunt

---
+++ HealthForm.Web/Gruntfile.js
@@ -0,0 +1,22 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        copy: {
+            build: {
+                cwd: 'Scripts/Vendors',
+                src: ['**/*.min.js'],
+                dest: 'Scripts/build',
+                expand: true,
+                flatten: true,
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['copy']);
+};
